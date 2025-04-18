2623b1507848ac3d4a161f2fd130dbeb0fad7236
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-04-18 21:49:25 +0200

    Revert "/items api barebones"
    This reverts commit 412f722e76abb9537f6969804a97e470739660b8.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const app = express()
 const path = require('path')
-const port = process.env.HTTPPORT || 3000
+const port = 3000
 
 const mongoose = require("mongoose")
-mongoose.connect('mongodb://127.0.0.1:27017/LinkManager');
+mongoose.connect('mongodb://127.0.0.1:27017',{useNewUrlParser: true,useUnifiedTopology:true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -16,16 +16,8 @@ app.get('/',(req , res)=>{
     res.sendFile(path.join(__dirname,'index.html'))
 })
 
-app.get('/items', async (req,res)=>{
-
-    try{
-        const items = await db.collection("links").find({}).toArray();
-        res.json(items);
-    }catch(error){
-        console.error("Error fetching data");
-        res.status(500).json({error: "Internal Server Errror"});
-    }
-
+app.get('/items',(req,res)=>{
+    res.json({message: "Get all items"});
 })
 
 app.listen(port, () => {
